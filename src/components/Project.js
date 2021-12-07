6b260edccfe622289855f49bc4486573f4d37464
melavirgule <melavirgule@gmail.com>
2021-12-07 13:38:10 +0100

    Different picker btn for web and mobile

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import {
   TextInput,
   View,
@@ -8,11 +8,19 @@ import {
   Image,
   Text,
 } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { homeStyle } from "../Styles/home";
 import * as DocumentPicker from "expo-document-picker";
 import * as MediaLibrary from "expo-media-library";
 // import DisplayDocs from "./DisplayDocs";
+import { AssetsSelector } from 'expo-images-picker';
+
+import { MediaType } from "expo-media-library";
+import { Ionicons } from '@expo/vector-icons';
+// import { AntDesign } from '@expo/vector-icons';
+
+
+
 
 const Projects = () => {
   const [textDescription, setTextDescription] = useState("");
@@ -36,6 +44,14 @@ const Projects = () => {
   // }, []);
 
   const handlePickDocs = async () => {
+    if (Platform.OS !== 'web') {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status !== 'granted') {
+        alert("Désolé, la permission est nécessaire pour accéder à la galerie");
+        return
+      }
+    }
+
     let pickerResult = await DocumentPicker.getDocumentAsync({
       type: "*/*",
       multiple: true,
@@ -46,45 +62,43 @@ const Projects = () => {
     }
     console.log(pickerResult);
     setSelectedDocs({ localUri: pickerResult.uri });
-    
-    // setSelectedDocs({
-    //   // localUri: pickerResult.uri,
-    //   // file: pickerResult.file.name,
-    //   output: pickerResult.output,
-    // });
-  };
+  
+  }
+
+  //     //     // setSelectedDocs({
+  //     //     //   // localUri: pickerResult.uri,
+  //     //     //   // file: pickerResult.file.name,
+  //     //     //   output: pickerResult.output,
+  //     //     // });
+  //   };
+  // }
   console.log({ selectedDocs });
 
-  // const selectedDocsArray = Object.entries(selectedDocs);
-  // selectedDocsArray.map((item, index) => {
-  //   console.log(item?.[1], index);
-  //   console.log(item?.[1]?.[0].name, index);
-  //   console.log(item?.[1]?.[1].name, index);
-  //   console.log(item?.[1]?.[2].name, index);
 
+  //   // const selectedDocsArray = Object.entries(selectedDocs);
+  //   // selectedDocsArray.map((item, index) => {
+  //   //   console.log(item?.[1], index);
+  //   //   console.log(item?.[1]?.[0].name, index);
+  //   //   console.log(item?.[1]?.[1].name, index);
+  //   //   console.log(item?.[1]?.[2].name, index);
 
-  // });
+  //   // });
 
-// const selectedDocsArray = Object.keys(selectedDocs).map(selectedDoc => {
-//     return{id: selectedDoc, name: selectedDocs[selectedDoc]}
-// });
+  // // const selectedDocsArray = Object.keys(selectedDocs).map(selectedDoc => {
+  // //     return{id: selectedDoc, name: selectedDocs[selectedDoc]}
+  // // });
 
+  // // const [ i, setI] = useState(item?.[""])
+  // // const [ j, setJ] = useState(item?.[""]?.[""])
 
-
-// const [ i, setI] = useState(item?.[""])
-// const [ j, setJ] = useState(item?.[""]?.[""])
-
-
-
-
-  // console.log(selectedDocsArray);
+  //   // console.log(selectedDocsArray);
 
   const DisplayPickedDocs = () => {
     if (selectedDocs !== null) {
       return (
         <View style={styles.container}>
           <Image
-            source={{ uri: selectedDocs.localUri}}
+            source={{ uri: selectedDocs?.localUri }}
             // source={require({ name: selectedDocsArray[setI][setJ].name})}
             // source={require({ name: selectedDocs?.[selectedDoc]})}
             style={styles.thumbnail}
@@ -111,10 +125,104 @@ const Projects = () => {
   //     ,
   // };
 
+
+  
+
+  const onSuccess = (data) => {
+    Alert.alert('Done', data.length + 'Images selected')
+  };
+
+  const widgetErrors = useMemo(
+    () => ({
+      errorTextColor: 'black',
+      errorMessages: {
+        hasErrorWithPermissions: "Veuillez authoriser l'application à accéder à votre galerie 🤗",
+        hasErrorWithLoading: 'Il y a eu un problème pendant le chargement des images 😬',
+        hasErrorWithResizing: 'Il y a eu un problème pendant le chargement des images 😬',
+        hasNoAssets: "Il n'y a pas d'images 😬",
+      },
+    }),
+    []
+  );
+
+  const widgetSettings = useMemo(
+    () => ({
+      getImageMetaData: true, // true might perform slower results but gives meta data and absolute path for ios users
+      initialLoad: 100,
+      assetsType: [MediaType.photo, MediaType.video],
+      minSelection: 1,
+      maxSelection: 6,
+      portraitCols: 4,
+      landscapeCols: 4,
+    }),
+    []
+  );
+
+  const widgetResize = useMemo(
+    () => ({
+      width: 50,
+      compress: 0.7,
+      base64: false,
+      saveTo: 'jpeg',
+    }),
+    []
+  );
+
+  const _textStyle = {
+    color: 'white',
+  };
+
+  const _buttonStyle = {
+    backgroundColor: 'orange',
+    borderRadius: 5,
+  };
+
+  const widgetNavigator = useMemo(
+    () => ({
+      Texts: {
+        finish: 'finish',
+        back: 'back',
+        selected: 'selected',
+      },
+      midTextColor: 'black',
+      minSelection: 1,
+      buttonTextStyle: _textStyle,
+      buttonStyle: _buttonStyle,
+      onBack: () => { },
+      onSuccess: (e) => onSuccess(e),
+    }),
+    []
+  );
+
+  const widgetStyles = useMemo(
+    () => ({
+      margin: 2,
+      bgColor: 'white',
+      spinnerColor: 'blue',
+      widgetWidth: 99,
+      videoIcon: {
+        Component: Ionicons,
+        iconName: 'ios-videocam',
+        color: 'tomato',
+        size: 20,
+      },
+      selectedIcon: {
+        Component: Ionicons,
+        iconName: 'ios-checkmark-circle-outline',
+        color: 'white',
+        bg: '#0eb14970',
+        size: 26,
+      },
+    }),
+    []
+  );
+
   // voir ce qu'il se passe pour le textarea
   return (
-    <SafeAreaView>
+    <SafeAreaProvider>
+      {/* <SafeAreaView> */}
       <View>
+
         <TextAreaDescription
           // onChangeText={(textDescription) => setTextDescription(textDescription)}
           // value={textDescription}
@@ -124,19 +232,53 @@ const Projects = () => {
           numberOfLines={3}
         />
 
-        <Button
+        {/* *********************Version Ordinateur********************* */}
+
+        {/* Si on est sur l'ordi on active document-picker,
+        Si on est sur mobile on active images-picker */}
+        {Platform.OS === 'web' ? <Button
           title="Choisissez vos documents"
           onPress={handlePickDocs}
           style={homeStyle.chooseButton}
-        />
-        {/* {selectedDocsArray !== null ? (
-          <DisplayPickedDocs />
-        ) : (
-          <Text>Vous n'avez pas sélectionné de documents</Text>
-        )} */}
-        <DisplayPickedDocs/>
+        /> : <AssetsSelector
+          Settings={widgetSettings}
+          Errors={widgetErrors}
+          Styles={widgetStyles}
+          Navigator={widgetNavigator}
+        />}
+        {/* <Button
+          title="Choisissez vos documents"
+          onPress={handlePickDocs}
+          style={homeStyle.chooseButton}
+        /> */}
+
+
+        {/* {selectedDocsArray !== null ? ( */}
+          {selectedDocs !== null ? (
+            <DisplayPickedDocs />
+          ) : (
+            <Text>Vous n'avez pas sélectionné de documents</Text>
+          )}
+        {/* <DisplayPickedDocs/> */}
+
+
+        {/* *********************Version Mobile********************* */}
+        {/* <Button
+            title="Choisissez vos documents"
+            onPress={handlePickDocs}
+            style={homeStyle.chooseButton}
+          /> */}
+
+        {/* <AssetsSelector
+          Settings={widgetSettings}
+          Errors={widgetErrors}
+          Styles={widgetStyles}
+          Navigator={widgetNavigator}
+        /> */}
+
       </View>
-    </SafeAreaView>
+      {/* </SafeAreaView> */}
+    </SafeAreaProvider>
   );
 };
 
